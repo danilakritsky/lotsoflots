206aab6028b1f8371cd9bc45da7608ce1b9b6f47
Danila Kritsky <danilakritsky@gmail.com>
2022-04-15 10:20:00 +0000

    Add auction page parser

--- scraper/items.py
+++ scraper/items.py
@@ -1,9 +1,17 @@
 """This module contains the model for scraped lot info."""
 
 import scrapy
-from itemloaders.processors import Identity, Join, TakeFirst
+from itemloaders.processors import Compose, Identity, Join, TakeFirst
 from scrapy import Field
 
 
 class LotItem(scrapy.Item):
-    auction_num = Field(output_processor=TakeFirst())
+    auction = Field(output_processor=TakeFirst())
+    auction_num = Field(
+        input_processor=Compose(lambda x: x[0], int), output_processor=TakeFirst()
+    )
+    lot_num = Field(
+        input_processor=Compose(lambda x: x[0], lambda x: x.strip("LOT"), int),
+        output_processor=TakeFirst(),
+    )
+    image_url = Field(output_processor=TakeFirst())
--- scraper/spiders/lot_spider.py
+++ scraper/spiders/lot_spider.py
@@ -1,4 +1,6 @@
 """The module container the spide that does the scraping of auction lots."""
+import copy
+import pdb
 import typing
 
 import scrapy
@@ -27,13 +29,40 @@ class LotSpider(scrapy.Spider):
 
         self.logger.info(f"Found current auction urls: {current_auctions_urls}")
         for url in current_auctions_urls:
-            loader = ItemLoader(item=LotItem(), selector=response)
-            loader.add_value("auction_num", url.split("/")[-1])
+            *_, auction, auction_num = url.split("/")
             yield response.follow(
-                url=url, callback=self.parse_auction_page, meta={"loader": loader}
+                url=url,
+                callback=self.parse_auction_page,
+                meta={"auction": auction, "auction_num": auction_num},
             )
 
     def parse_auction_page(self, response: Response) -> typing.Union[Request, LotItem]:
         """Parse auction page to get info on lots."""
-        loader = response.meta['loader']
-        yield loader.load_item()
+        lots = response.css(".listInner")
+
+        for lot in lots:
+            # create new loader for every item here
+            # since passing loader by meta from parse() raises the "Can't picke Selector objects" error
+            # @ https://stackoverflow.com/questions/49659261/scrapy-cloud-spider-requests-fail-with-generatorexit
+            loader = ItemLoader(item=LotItem(), selector=lot)
+            loader.add_value("auction", response.meta["auction"])
+            loader.add_value("auction_num", response.meta["auction_num"])
+            loader.add_css("lot_num", ".listLot::text")
+            loader.add_css("image_url", "[src]::attr(src)")
+
+            lot_url = lot.css("[data-link]::attr(data-link)").get()
+            yield response.follow(
+                url=lot_url, callback=self.parse_lot_page, meta={"loader": loader}
+            )
+
+    def parse_lot_page(self, response: Response) -> LotItem:
+        """Extract lot information from its page."""
+        loader = response.meta["loader"]
+        loader.selector = response.body
+        return loader.load_item()
+
+
+def save_result(response: Response, filename="lots_scraped.html") -> None:
+    with open(filename, "a+") as f:
+        f.write(response)
+    print(f"Data saved to {filename}.")
